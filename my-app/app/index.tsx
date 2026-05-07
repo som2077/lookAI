b9f14dead30cb8dbf95dfa5abcaea8bda0089c24
som2077 <somgautam2077@gmail.com>
2026-05-07 20:35:43 +0530

    feat: integrate AppGradientBackground in Index component and update button styles; add Terms of Service and Privacy Policy notice

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -2,6 +2,7 @@ import { useAuth } from "@clerk/clerk-expo";
 import { Href, Redirect, useRouter } from "expo-router";
 import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { AppGradientBackground } from "../components/ui/AppGradientBackground";
 
 export default function Index() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -20,7 +21,8 @@ export default function Index() {
   }
 
   return (
-    <SafeAreaView className="flex-1 bg-white">
+    <AppGradientBackground>
+    <SafeAreaView className="flex-1">
       <View className="flex-1 items-center justify-center px-6">
         <Text className="text-3xl font-bold text-gray-800 mb-6">
           Get Started
@@ -28,11 +30,16 @@ export default function Index() {
 
         <TouchableOpacity
           onPress={() => router.push("/(auth)/sign-in" as Href)}
-          className="w-full bg-blue-600 py-4 rounded-xl items-center"
+          className="w-full bg-[#1E1A24] py-4 rounded-xl items-center"
         >
           <Text className="text-white font-bold text-base">Get Started</Text>
         </TouchableOpacity>
+
+        <Text>
+        By continuing, you accept our Terms of Service and acknowledge our Privacy Policy. You can tap them to view details.
+        </Text>
       </View>
     </SafeAreaView>
+    </AppGradientBackground>
   );
 }
