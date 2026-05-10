6acccc535e05f88a0b26e223ccec8a1269c12e60
som2077 <somgautam2077@gmail.com>
2026-05-10 16:28:44 +0530

    chore: remove Supabase integration and related onboarding files

--- my-app/db/drizzle.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
-const connectionString = process.env.EXPO_PUBLIC_SUPABASE_DB_URL!;
-const client = postgres(connectionString);
-
-export const db = drizzle(client);
